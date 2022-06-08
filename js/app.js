d564dfbbb2199d5e1964adaaedc5c0c70ec7f386
Pierce Petit <ppetit2496@gmail.com>
2022-06-07 21:51:20 -0500

    add cards for dev work commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log("")
+
+
+
+import { projectData } from "./project-data.js"
+console.log(projectData)
+
+const cardContainer = document.getElementById('card-container')
+
+let projectMarkup = projectData.map(project =>
+    `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${project.image}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${project.title}</h5>
+                        <p class="card-text">${project.info}</p>
+                        <div>
+                            <a href="${project.github}" class="btn btn-primary">Github</a>
+                            <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+).join('')
+
+cardContainer.innerHTML = projectMarkup
+console.log(projectMarkup)
--- js/project-data.js
+++ js/project-data.js
@@ -1,9 +1,13 @@
 let projectData = [
     {
-        title: ,
-        deployment: ,
-        image: ,
-        github: ,
-        info: ,
+        title: 'Tic Tac Toe',
+        deployment:'https://ttt-whatitdo.netlify.app',
+        image: "assets/images/project-pics/tictactoe.jpeg",
+        github: 'https://github.com/piercepetit7/ttt-weekend' ,
+        info:'Tic Tac Toe is a small game that shows simple HTML, CSS, and JavaScript' ,
     },
 ]
+
+export {
+    projectData 
+}
