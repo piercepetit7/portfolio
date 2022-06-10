e0546f83ec9c5bc477e585b563d484936a636107
Pierce Petit <ppetit2496@gmail.com>
2022-06-10 01:02:08 -0500

    fix pic commit

--- js/app.js
+++ js/app.js
@@ -115,3 +115,9 @@ spotify.addEventListener('click', spotifyClick)
 function spotifyClick() {
     window.open("https://open.spotify.com/user/piercepetit7?si=RH02uPHPSBON1_mRSYCH6Q");
 }
+
+
+//
+$('.carousel').carousel({
+    interval: false,
+});
